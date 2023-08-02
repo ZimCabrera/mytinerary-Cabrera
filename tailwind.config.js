b3fecdf6d8294001a2aa0ff29f28d042eaab05a5
Cynthia M. Cabrera (ZiM) <cynthiacabrera1088@gmail.com>
2023-08-02 00:09:24 -0300

    configurando Taildwin

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './src/**/*.(js,jxs,ts,tsx)'
+  ],
+  theme: {
+     extend: {}
+  },
+  variants: {},
+  plugins: [],
+}
+
